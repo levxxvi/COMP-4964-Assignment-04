aa17c518d1a05088e93c546f470d3e9e936443e8
Alexandra Wong <lex115wong@gmail.com>
2024-11-12 23:25:35 -0800

    Commit

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Ensure the scripts are executable
+chmod +x /opt/codedeploy-agent/deployment-root/*/scripts/before_install.sh
+chmod +x /opt/codedeploy-agent/deployment-root/*/scripts/after_install.sh
+
 # Create or clear the target directory
 sudo mkdir -p /var/www/html
 sudo rm -rf /var/www/html/*
